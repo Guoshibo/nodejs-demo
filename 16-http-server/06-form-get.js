19e162fcfbab7b07cf93e123b58237abdb80d562
wangding <408542507@qq.com>
2018-11-27 16:26:23 +0800

    new

--- 16-http-server/06-form-get.js
+++ 16-http-server/06-form-get.js
@@ -6,40 +6,38 @@ const http = require('http'),
 
 var items=[];
 
-http.createServer((req,res)=>{
-  //if(req.url==='/' && req.method === 'GET')
-  //点击按钮时候会报错
-  if(url.parse(req.url).pathname === '/' && req.method === 'GET')
-{
-  console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
-  console.log(req.headers);
-  console.log('');
-  //var data = url.parse(req.url).query;
- // items.push(data.item);
- 
-  var data = qs.parse(url.parse(req.url).query);
-  console.log('query string:',url.parse(req.url).query);
-  console.log('data:',data);
-  req.pipe(process.stdout);
-  var html=''
-  +'<!DOCTYPE html>'
-  +"<html lang='en'>"
-  +'<head>'
-  +'<meta charset="utf=8">'
-  +'<title>Todo list</title>'
-  +'</head>'
-  +'<body>'
-  +'<h1>Todo list</h1>'
-  +"<from method='get' action='/'>"
-  +"<input type='text' name='item'/>"
-  +"<input type='button' value='Add item'/>"
-  +'</from>'
-  +'</body>'
-  +'</html>';
-  res.end(html);
-}else{
-  res.statusCode = 404;
-  res.end('Error!');
-}
+http.createServer((req, res) => {
+    var path = url.parse(req.url).pathname;
+
+    if(path !== '/') {
+      err(res);
+      return;          
+    }
+    console.log(req.headers);
+    console.log('');
+    add(req, res);
 }).listen(8080);
 
+function show(res) {
+    var html = '<!DOCTYPE html>\n'+ '<html>\n'+'<head>\n'+"<meta charset='utf8'>\n"+'<title>Todo list</title>\n'
+      +'</head>\n'+'<body>\n'+'<h1>Todo List</h1>\n'+'<ul>\n'+items.map(function(item) {return'<li>' + item + '</li>';}).join('\n')+'</ul>\n'+'<form method="get" action="/">\n'+'<p><input type="text" name="item"/>'+'<input type="submit" value="Add Item" /></p>\n'+'</form>\n'+'</body>\n'+'</html>';
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Content-Length', Buffer.byteLength(html));
+      res.statusCode = 200;
+      res.end(html);
+}
+function add(req, res) {
+    var value = qs.parse(url.parse(req.url).query).item;
+    if(typeof value !== 'undefined') {
+      items.push(value);
+    }  
+      show(res);
+    }
+
+function err(res) {
+    var msg = 'Not found!';
+    res.statusCode = 404;
+    res.setHeader('Content-Length', msg.length);
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(msg);
+}
